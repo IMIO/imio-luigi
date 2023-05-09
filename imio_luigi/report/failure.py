c2b81585ff9170ec987933db2a99f817722df4cd
Martin Peeters <martin.peeters@affinitic.be>
2023-05-09 13:38:20 +0200

    Improve failure report to print the number of occurence

--- imio_luigi/report/failure.py
+++ imio_luigi/report/failure.py
@@ -10,6 +10,26 @@ def cli():
     pass
 
 
+def print_report(dirname):
+    result = {}
+    click.echo(f"Failures for {dirname}")
+    for filename in os.listdir(dirname):
+        fpath = os.path.join(dirname, filename)
+        with open(fpath, "r") as f:
+            data = json.load(f)
+            if "reference" in data:
+                msg = f"{data['reference']}: {data['error']}"
+            else:
+                msg = f"{data['error']}"
+            if msg not in result:
+                result[msg] = 1
+            else:
+                result[msg] += 1
+    sorted_result = sorted([(v, k) for k, v in result.items()], reverse=True)
+    for nbr, msg in sorted_result:
+        click.echo(f"{nbr}: {msg}")
+
+
 @cli.command()
 def list():
     """List tasks in error"""
@@ -22,15 +42,12 @@ def list():
 @click.argument("task-name")
 def report(task_name):
     """List errors for a given task"""
-    dirname = f"./failures/{task_name}"
-    for filename in os.listdir(dirname):
-        fpath = os.path.join(dirname, filename)
-        with open(fpath, "r") as f:
-            data = json.load(f)
-            if "reference" in data:
-                click.echo(f"{data['reference']}: {data['error']}")
-            else:
-                click.echo(f"{data['error']}")
+    if task_name == "all":
+        dirnames = [f"./failures/{f}" for f in os.listdir("./failures")]
+    else:
+        dirnames = [f"./failures/{task_name}"]
+    for dirname in dirnames:
+        print_report(dirname)
 
 
 def main():
