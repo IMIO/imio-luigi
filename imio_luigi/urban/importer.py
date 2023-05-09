af202ccfc02d5e487e011417097b49d544044776
Martin Peeters <martin.peeters@affinitic.be>
2023-05-09 15:51:34 +0200

    Add attachment data and set destination folder

--- imio_luigi/urban/importer.py
+++ imio_luigi/urban/importer.py
@@ -2,6 +2,7 @@
 
 from imio_luigi import core
 
+import base64
 import json
 import logging
 import luigi
@@ -18,6 +19,41 @@ class GetFiles(core.WalkFS):
     login = luigi.Parameter()
     password = luigi.Parameter()
 
+    def _get_url(self, data):
+        """Construct POST url based on type"""
+        mapping = {
+            "BuildLicence": "buildlicences",
+            "CODT_BuildLicence": "codt_buildlicences",
+            "Article127": "article127s",
+            "IntegratedLicence": "integratedlicences",
+            "CODT_IntegratedLicence": "codt_integratedlicences",
+            "UniqueLicence": "uniquelicences",
+            "CODT_UniqueLicence": "codt_uniquelicences",
+            "Declaration": "declarations",
+            "UrbanCertificateOne": "urbancertificateones",
+            "CODT_UrbanCertificateOne": "codt_urbancertificateones",
+            "UrbanCertificateTwo": "urbancertificatetwos",
+            "CODT_UrbanCertificateTwo": "codt_urbancertificatetwos",
+            "PreliminaryNotice": "preliminarynotices",
+            "EnvClassOne": "envclassones",
+            "EnvClassTwo": "envclasstwos",
+            "EnvClassThree": "envclassthrees",
+            "ParcelOutLicence": "parceloutlicences",
+            "CODT_ParcelOutLicence": "codt_parceloutlicences",
+            "MiscDemand": "miscdemands",
+            "NotaryLetter": "notaryletters",
+            "CODT_NotaryLetter": "codt_notaryletters",
+            "Inspection": "inspections",
+            "Ticket": "tickets",
+            "Architect": "architects",
+            "Geometrician": "geometricians",
+            "Notary": "notaries",
+            "Parcelling": "parcellings",
+            "Division": "divisions",
+        }
+        folder = mapping[data["type"]]
+        return f"{self.url}/{folder}"
+
     def run(self):
         for fpath in self.filepaths:
             with open(fpath, "r") as f:
@@ -25,7 +61,7 @@ class GetFiles(core.WalkFS):
                 yield RESTPost(
                     key=content["reference"],
                     data=content,
-                    url=self.url,
+                    url=self._get_url(content),
                     login=self.login,
                     password=self.password,
                 )
@@ -50,9 +86,20 @@ class RESTPost(core.PostRESTTask):
     def test_parameters(self):
         return {"portal_type": self.data["@type"], "getReference": self.key}
 
+    def _add_attachments(self, data):
+        if "__children__" not in data:
+            return data
+        for child in data["__children__"]:
+            if "file" not in child:
+                continue
+            with open(child["file"]["data"], "rb") as f:
+                content = f.read()
+                child["file"]["data"] = base64.b64encode(content).decode("utf8")
+        return data
+
     @property
     def json_body(self):
-        return core.frozendict_to_dict(self.data)
+        return self._add_attachments(core.frozendict_to_dict(self.data))
 
     def complete(self):
         r = self.test_complete()
