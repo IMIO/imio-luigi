f1c9b97749fb826be2b2afc15db058d0f0a0eed6
jimbiscuit <julienchandelle@gmail.com>
2023-12-05 14:29:30 +0100

    Add dtype and nan handler in csv task

--- imio_luigi/core/task_csv.py
+++ imio_luigi/core/task_csv.py
@@ -5,28 +5,32 @@ from datetime import datetime
 import abc
 import luigi
 import pandas as pd
+import numpy as np
 
 class GetFromCSVFile(luigi.Task):
     columns = ["*"]
     delimiter= ","
+    dtype={}
 
     @property
     @abc.abstractmethod
     def filepath(self):
         """The path to the table json dump"""
         return None
-    
+
     def _col_filter(self, col):
         return self.columns == ["*"] or col in self.columns
-    
+
     def query(self, min_range=None, max_range=None):
         """Return each row as a dict object"""
         pdata = pd.read_csv(
             self.filepath,
             delimiter=self.delimiter,
             usecols=self._col_filter,
+            dtype=self.dtype
         )
-        
+        pdata = pdata.replace({np.nan: None})
+
         for nbr, line in pdata.iterrows():
             if min_range and nbr < min_range:
                 continue
@@ -35,3 +39,11 @@ class GetFromCSVFile(luigi.Task):
             data = line.to_dict()
             data["key"] = nbr
             yield data
+
+    def log_failure_output(self):
+        fname = self.task_id.split("_")[-1]
+        fpath = (
+            f"./failures/{self.task_namespace}-"
+            f"{self.__class__.__name__}/{fname}.json"
+        )
+        return luigi.LocalTarget(fpath)
