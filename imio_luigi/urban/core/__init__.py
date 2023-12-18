870fa3d6e016e16aec5ec396871c8b478804054b
jimbiscuit <julienchandelle@gmail.com>
2023-12-18 08:53:56 +0100

    Update init file

--- imio_luigi/urban/core/__init__.py
+++ imio_luigi/urban/core/__init__.py
@@ -1,12 +1,22 @@
-from imio_luigi.urban.core.add_data import AddNISData
-from imio_luigi.urban.core.resolver import UrbanEventConfigUidResolver
-from imio_luigi.urban.core.mapping import UrbanTransitionMapping
+from imio_luigi.urban.core.task_add_data import ( AddNISData, AddUrbanEvent )
+from imio_luigi.urban.core.task_resolver import UrbanEventConfigUidResolver
+from imio_luigi.urban.core.task_mapping import UrbanTransitionMapping
 from imio_luigi.urban.core.config import ( config, workflows)
+from imio_luigi.urban.core.task_transform import (
+    TransformWorkLocation, 
+    TransformCadastre,
+    TransformArchitect
+)
+
 
 __all__ = (
     "config",
     "workflows"
     "AddNISData",
+    "AddUrbanEvent",
     "UrbanEventConfigUidResolver",
-    "UrbanTransitionMapping"
+    "UrbanTransitionMapping",
+    "TransformWorkLocation",
+    "TransformCadastre",
+    "TransformArchitect"
 )
