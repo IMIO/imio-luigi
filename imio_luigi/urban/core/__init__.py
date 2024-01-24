929de30971d35430eab49228d60202bfd078e2c1
jimbiscuit <julienchandelle@gmail.com>
2024-01-24 10:55:14 +0100

    Add UrbanTypeMapping in init

--- imio_luigi/urban/core/__init__.py
+++ imio_luigi/urban/core/__init__.py
@@ -1,6 +1,6 @@
 from imio_luigi.urban.core.task_add_data import ( AddNISData, AddUrbanEvent )
 from imio_luigi.urban.core.task_resolver import UrbanEventConfigUidResolver
-from imio_luigi.urban.core.task_mapping import UrbanTransitionMapping
+from imio_luigi.urban.core.task_mapping import (UrbanTransitionMapping, UrbanTypeMapping)
 from imio_luigi.urban.core.config import ( config, workflows)
 from imio_luigi.urban.core.task_transform import (
     TransformWorkLocation, 
@@ -18,5 +18,6 @@ __all__ = (
     "UrbanTransitionMapping",
     "TransformWorkLocation",
     "TransformCadastre",
-    "TransformArchitect"
+    "TransformArchitect",
+    "UrbanTypeMapping"
 )
