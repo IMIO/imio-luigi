d9d2d2dac150b2dfdff15cb12b788fd57b96b9b0
jimbiscuit <julienchandelle@gmail.com>
2024-05-06 12:41:11 +0200

    Add missing export in urban core init

--- imio_luigi/urban/core/__init__.py
+++ imio_luigi/urban/core/__init__.py
@@ -1,4 +1,4 @@
-from imio_luigi.urban.core.task_add_data import AddNISData, AddUrbanEvent, CreateApplicant
+from imio_luigi.urban.core.task_add_data import AddNISData, AddUrbanEvent, CreateApplicant, AddAllOtherEvents, AddUrbanOpinion
 from imio_luigi.urban.core.task_resolver import UrbanEventConfigUidResolver
 from imio_luigi.urban.core.task_mapping import UrbanTransitionMapping, UrbanTypeMapping
 from imio_luigi.urban.core.config import config, workflows
@@ -14,6 +14,8 @@ __all__ = (
     "workflows",
     "AddNISData",
     "AddUrbanEvent",
+    "AddAllOtherEvents",
+    "AddUrbanOpinion"
     "UrbanEventConfigUidResolver",
     "UrbanTransitionMapping",
     "TransformWorkLocation",
