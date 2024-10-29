33b668bac4fc66a915864598cc6bb878fb6c121f
jimbiscuit <julienchandelle@gmail.com>
2024-10-29 12:46:42 +0100

    Change urban core init

--- imio_luigi/urban/core/__init__.py
+++ imio_luigi/urban/core/__init__.py
@@ -1,11 +1,20 @@
-from imio_luigi.urban.core.task_add_data import AddNISData, AddUrbanEvent, CreateApplicant, AddAllOtherEvents, AddUrbanOpinion, AddValuesInDescription
-from imio_luigi.urban.core.task_resolver import UrbanEventConfigUidResolver
-from imio_luigi.urban.core.task_mapping import UrbanTransitionMapping, UrbanTypeMapping
 from imio_luigi.urban.core.config import config, workflows
+from imio_luigi.urban.core.task_add_data import (
+    AddAllOtherEvents,
+    AddEvents,
+    AddNISData,
+    AddUrbanEvent,
+    AddUrbanOpinion,
+    AddValuesInDescription,
+    CreateApplicant,
+)
+from imio_luigi.urban.core.task_mapping import UrbanTransitionMapping, UrbanTypeMapping
+from imio_luigi.urban.core.task_resolver import UrbanEventConfigUidResolver
 from imio_luigi.urban.core.task_transform import (
-    TransformWorkLocation,
     TransformCadastre,
     TransformContact,
+    TransformWorkLocation,
+    TransformWorkLocationMultiParams,
 )
 
 
@@ -23,5 +32,7 @@ __all__ = (
     "TransformContact",
     "UrbanTypeMapping",
     "CreateApplicant",
-    "AddValuesInDescription"
+    "AddValuesInDescription",
+    "TransformWorkLocationMultiParams",
+    "AddEvents"
 )
