03b58649f0175e74b346143236f759b9f6d56819
jimbiscuit <julienchandelle@gmail.com>
2025-03-04 13:50:24 +0100

    Reorganize utils code

--- imio_luigi/utils/__init__.py
+++ imio_luigi/utils/__init__.py
@@ -4,7 +4,12 @@ from imio_luigi.utils.helpers import (
     add_data_to_description,
     fix_search_term,
     get_value_from_path,
-    get_value_from_path_with_parents
+    get_value_from_path_with_parents,
+    calculate_similarity,
+    find_most_similar_result,
+    find_most_similar_term,
+    find_match,
+    find_result_similarity
 )
 from imio_luigi.utils.mapping import MappingCountryInMemoryTask, MappingCountryTask
 from imio_luigi.utils.summary import (
@@ -22,6 +27,11 @@ __all__ = (
     "get_all_keys",
     "get_value_from_path",
     "get_value_from_path_with_parents",
+    "calculate_similarity",
+    "find_most_similar_result",
+    "find_most_similar_term",
+    "find_match",
+    "find_result_similarity",
     "get_all_unique_values_with_first_ref",
     "get_all_unique_value_with_callback",
     "fix_search_term",
--- imio_luigi/utils/helpers.py
+++ imio_luigi/utils/helpers.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import os
+from difflib import SequenceMatcher
+from statistics import mean
+
+
+SIMILARITY_THRESHOLD_BETWEEN_RESULTS_WITH_WARNING = 0.9
+SIMILARITY_THRESHOLD_BETWEEN_RESULTS_WITHOUT_WARNING = 1
+SIMILARITY_THRESHOLD_WITH_TERM_WITH_WARNING = 0.9
+SIMILARITY_THRESHOLD_WITH_TERM_WITHOUT_WARNING = 1
 
 def get_value_from_path(data, path):
+    """
+    Return value from a dict with a path seprate with /
+
+    :param data: data where to search the value
+    :type data: dict
+    :param path: path to the data
+    :type path: string
+    :return: value or values correspondting to the path found in the data
+    :rtype: string or list of string
+    """
     path_split = path.split("/")
     current_data = data
     for key in path_split:
@@ -60,3 +78,78 @@ def add_data_to_description(data, value):
         }
     data["description"]["data"] += value
     return data
+
+
+def calculate_similarity(str1, str2):
+    seq_matcher = SequenceMatcher(None, str1, str2)
+    return seq_matcher.ratio()
+
+
+def get_value_conditional(data, key=None):
+    if not key:
+        return data
+    return data[key]
+
+
+def find_most_similar_result(results, key=None):
+    num_results = len(results)
+
+    similarity_list = []
+
+    for i in range(num_results):
+        for j in range(i + 1, num_results):
+            similarity_list.append(
+                calculate_similarity(
+                    get_value_conditional(results[i], key),
+                    get_value_conditional(results[j], key)
+                )
+            )
+
+    if all(filter(lambda a: a > SIMILARITY_THRESHOLD_BETWEEN_RESULTS_WITHOUT_WARNING, similarity_list)):
+        return True, None
+    elif all(filter(lambda a: a > SIMILARITY_THRESHOLD_BETWEEN_RESULTS_WITH_WARNING, similarity_list)):
+        return True, similarity_list
+    else:
+        return False, f"Pas assez d'assurance qu'il y a de la ressemblance entre les resultats trouvé."
+
+
+def find_most_similar_term(term, results, key=None):
+    highest_similarity = 0
+    most_similar_result = None
+
+    for result in results:
+        similarity_ratio = calculate_similarity(term, get_value_conditional(result, key))
+
+        if similarity_ratio > highest_similarity:
+            highest_similarity = similarity_ratio
+            most_similar_result = result
+
+    return most_similar_result, highest_similarity
+
+
+def find_match(values, expected_value, key=None):
+    exact_match = []
+    for item in values:
+        name = get_value_conditional(item, key).split("(")[0].strip()
+        if name.upper() == expected_value.strip().upper():
+            exact_match.append(item)
+    if len(exact_match) == 1:
+        return exact_match[0]
+    return []
+
+
+def find_result_similarity(results, term=None, key=None):
+
+    similarity_between_results, error = find_most_similar_result(results, key=key)
+
+    if similarity_between_results:
+        return results[0], error
+
+    if term:
+        most_similar_result, highest_similarity = find_most_similar_term(term, results, key=key)
+        if highest_similarity > SIMILARITY_THRESHOLD_WITH_TERM_WITHOUT_WARNING:
+            return most_similar_result, None
+        elif highest_similarity > SIMILARITY_THRESHOLD_WITH_TERM_WITH_WARNING:
+            return most_similar_result, highest_similarity
+
+    return None, None
