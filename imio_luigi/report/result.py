0eade59ba41dacc202494725345592756f604543
jimbiscuit <julienchandelle@gmail.com>
2025-03-07 14:31:17 +0100

    Update and add doc to report result script

--- imio_luigi/report/result.py
+++ imio_luigi/report/result.py
@@ -27,21 +27,29 @@ def get_value(data, path):
     return current_data
 
 
-def match_value(value, check):
-    match = re.match(check, value)
+def match_value(value, check, check_type="match"):
+    if value is check:
+        return True
+    if value is None:
+        return False
+    if check_type == "search":
+        match = re.search(check, value)
+    else:
+        match = re.match(check, value)
+
     return bool(match)
 
 
-def check_value(value, check=None):
+def check_value(value, check=None, check_type="match"):
     if isinstance(value, list):
         if check:
-            return [1 for item in value if match_value(item, check)]
+            return [1 for item in value if match_value(item, check, check_type)]
         else:
             return [
                 1 if item is not None and item != "" else 0 for item in value
             ]
     if check:
-        return [1 if match_value(value, check) else 0]
+        return [1 if match_value(value, check, check_type) else 0]
     else:
         return [1]
 
@@ -70,12 +78,23 @@ def get_stat_data(path, configs, select=None):
             data_value = get_value(data, key)
             if not data_value:
                 continue
-            value_to_chek = config.get("value", None)
-            result = sum(check_value(data_value, value_to_chek))
-            output[get_title_config(config)]["results"][data["reference"]] = {
+            value_to_check = config.get("value", None)
+            check_type = config.get("check_type", None)
+            result = sum(
+                check_value(
+                    data_value,
+                    check=value_to_check,
+                    check_type=check_type,
+                )
+            )
+            output_value = {
                 "path": filename,
                 "result": result
             }
+            value_to_count = config.get("value_to_count", None)
+            if value_to_count:
+                output_value["value_to_count"] = get_value(data, value_to_count)
+            output[get_title_config(config)]["results"][data["reference"]] = output_value
     return output, files
 
 
@@ -106,32 +125,53 @@ def print_unique_values(values, total):
 @click.argument("path")
 @click.argument("config")
 def stat(path, config):
-    """Display stat of a series of key for all result"""
+    """
+    Display stat of a series of key for all result
+
+    PATH Path to result folder
+
+    CONFIG Path to stat JSON config file with list of config
+
+    List of option in config file:
+
+    - id: optional, id of the config, if present will be use as title
+
+    - key: required, key in data that will be use as check to do stat, if id no present will be use as title
+
+    - value: optional, value as regex pattern will be match (default) against the value in data
+
+    - check_type: optional, default is match, can be switch to search, use to determine the method use to check the value
+
+    - type: optional, can be set to list, it will return stat about ocurence
+
+    - value_to_count: optional, when type is set to list, a specific value can be choose to have count of it instead of occurence 
+
+    """
     with open(config, "r") as f:
         configs = json.load(f)
     output, files = get_stat_data(path, configs)
 
     click.echo(f"Stat (for {len(files)} files):")
 
-    for out in output:
-        title = out
+    for title in output:
         click.echo(f"\t{title}:")
         total_count = len(files)
-        values = output[out]["results"]
-        result_type = output[out]["type"]
-        list_values = [value["result"] for value in values.values() if value["result"] >= 1]
+        values = output[title]["results"]
+        result_type = output[title]["type"]
+        list_values = [value.get("value_to_count", value["result"]) for value in values.values() if value["result"] >= 1]
         count = len(list_values)
         click.echo(f"\t\tCount: {count}")
         percentage = (count / total_count) * 100
         click.echo(f"\t\tPercentage: {percentage:.2f}%")
         if len(list_values) < 1:
             continue
-        min_value = min(list_values)
-        max_value = max(list_values)
-        click.echo(f"\t\tMin/Max: {min_value} / {max_value}")
-        median_value = median(list_values)
-        mean_value = fmean(list_values)
-        click.echo(f"\t\tMedian/Average: {median_value:.2f} / {mean_value:.2f}")
+        if all([isinstance(value, int) for value in list_values]):
+            min_value = min(list_values)
+            max_value = max(list_values)
+            click.echo(f"\t\tMin/Max: {min_value} / {max_value}")
+            median_value = median(list_values)
+            mean_value = fmean(list_values)
+            click.echo(f"\t\tMedian/Average: {median_value:.2f} / {mean_value:.2f}")
         if result_type == "list":
             value_by_count = unique_value_count(list_values)
             print_unique_values(value_by_count, total_count)
@@ -142,6 +182,23 @@ def stat(path, config):
 @click.argument("config")
 @click.argument("filter-path")
 def filter(path, config, filter_path):
+    """
+    Apply filter on a config stat to return finer stat
+
+    PATH Path to result folder
+
+    CONFIG Path to stat JSON config file with list of config
+
+    FILTER_PATH Path to JSON filter file with a dict config
+
+    List of option in filter file:
+
+    - key: required, the key (or id) of the config to apply the filter
+
+    - value: required, value that will be use in filter operation
+
+    - operator: required, the operator use during operation of the filter
+    """
     with open(config, "r") as f:
         configs = json.load(f)
     with open(filter_path, "r") as f:
@@ -168,7 +225,13 @@ def filter(path, config, filter_path):
 @click.option("--unique", default=False, is_flag=True, help="Only show unique key")
 @click.option("--count", default=False, is_flag=True, help="Show count occurence when unique selected")
 def list_key(key, path, display_no_key, unique, count):
-    """List value of a specifc key in json result"""
+    """
+    List value of a specifc key in json result
+
+    KEY key that will be display
+
+    PATH Path to result folder
+    """
     files = os.listdir(path)
     output = []
     no_key = []
